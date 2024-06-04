8b34c91089de486ddc4786f1f58b3f6dd9a134fe
lenguyenhcm325 <lenguyenhcm325@gmail.com>
2024-06-04 21:24:22 +0200

    add backend java template from spring initializr

---
+++ backend/src/main/java/com/github/lenguyenhcm325/coursemanagementsystembackend/CoursemanagementsystembackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.lenguyenhcm325.coursemanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemanagementsystembackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemanagementsystembackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/lenguyenhcm325/coursemanagementsystembackend/CoursemanagementsystembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lenguyenhcm325.coursemanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemanagementsystembackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
